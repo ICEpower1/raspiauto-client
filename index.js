cc1d879cad7d93c40641f2f6bbdcfcdf91475447
stutommi <tommi.teetee@hotmail.com>
2020-07-19 20:01:42 +0300

    tweak

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+console.log("0")
 var net = require('net');
 
-console.log("HALOJATAHALLOOO")
+console.log("1")
 
 var client = new net.Socket();
+console.log("2")
 client.connect(8000, '86.115.196.9', function() {
     console.log('Connected');
     client.write('Hello, server! Love, Client.');
 });
+console.log("3")
 
 client.on('data', function(data) {
     console.log('Received: ' + data);
     client.destroy(); // kill client after server's response
 });
+console.log("4")
